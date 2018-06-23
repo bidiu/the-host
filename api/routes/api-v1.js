cf19e71d1cb4ec8b7e72b44e3e08bc9411f40943
Eric Ma <ericmic@outlook.com>
2018-06-23 12:34:16 -0400

    venue updated

--- api/routes/api-v1.js
+++ api/routes/api-v1.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const userController = require('../controllers/user');
+const venueController = require('../controllers/venue');
 
 const asyncWrapper = func =>
   async (req, res, next) => {
@@ -20,5 +21,15 @@ router.get('/users/:userId', asyncWrapper(userController.retrieve));
 router.post('/users', asyncWrapper(userController.create));
 
 router.patch('/users/:userId', asyncWrapper(userController.update));
+
+
+/*
+ * venue related routes start
+ */
+router.get('/venues/:venueId', asyncWrapper(venueController.retrieve));
+
+router.post('/venues', asyncWrapper(venueController.create));
+
+router.patch('/venues/:venueId', asyncWrapper(venueController.update));
 
 module.exports = router;
