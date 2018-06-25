472589f3bb858525e632a0bf0e14db74ea621593
bedew <sunhe1007@126.com>
2018-06-25 10:38:27 -0400

    made the venue page display dynamic data

--- app/assets/js/common.js
+++ app/assets/js/common.js
@@ -26,3 +26,29 @@ axios.defaults.withCredentials = true;
 function getCurUser() {
   return Cookies.getJSON('user');
 }
+
+/**
+ * Example:
+ *    Suppose current page's url is
+ * 
+ *      http://localhost:3000/html/venue.html?venueId=1
+ *    then, this function will return:
+ * 
+ *      1 -----> always a string!!!
+ * 
+ * Note that the `name` param is case sentitive.
+ * If you give a non-existant name, `null` will be returned.
+ * 
+ * @param {*} name  the query parameter name whose value you want to get
+ * @param {*} url   for getting query for current page, do NOT pass this
+ */
+function getQueryParamVal(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+  const results = regex.exec(url);
+
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
