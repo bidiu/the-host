7d1acc59b34ae341c863a598f1f2729656a97e63
bedew <sunhe1007@126.com>
2018-07-05 17:04:21 -0400

    styles the search box on home page

--- app/assets/js/ui/SimpleSearchBox.js
+++ app/assets/js/ui/SimpleSearchBox.js
@@ -1,45 +1,47 @@
-class SimpleSearchBox extends React.Component {
+class _SimpleSearchBox extends React.Component {
   constructor(props) {
     super(props);
     this.submitHandler = this.submitHandler.bind(this);
     this.inputChangeHandler = this.inputChangeHandler.bind(this);
 
-    this.state = { venueType: '' };
+    this.state = { venueName: '' };
   }
 
   inputChangeHandler(event) {
     let { value } = event.target;
 
-    this.setState({ venueType: value });
+    this.setState({ venueName: value });
   }
 
   submitHandler(event) {
     event.preventDefault();
 
-    let { venueType } = this.state;
-    venueType = venueType.trim();
-    if (venueType) {
-      window.location = `/html/search.html?type=${venueType}`;
+    let { venueName } = this.state;
+    venueName = venueName.trim();
+
+    if (venueName) {
+      window.location = `/html/search.html?name=${venueName}`;
+    } else {
+      let message = 'Please input something before searching.';
+      this.props.addNotification(new NotificationEntry({ message, timeout: 3000 }));
     } 
   }
   
   render() {
-    let { venueType } = this.state;
-
-    let btnStyle = {
-      backgroundColor: '#aaa',
-      width: '100px',
-      height: '46px'
-    };
+    let { venueName } = this.state;
 
     return (
-      
-      <form className="SimpleSearchBox" onSubmit={this.submitHandler}>
-        <input type="text" placeholder="Search a venue here !" value={venueType} onChange={this.inputChangeHandler}/>
-        <input type="submit" value="Search" />
-        <i className="fa fa-search fa-lg fa-fw" aria-hidden="true"></i>
-      </form>
-    
+      <div className="SimpleSearchBox" onSubmit={this.submitHandler}>
+        <input type="text" placeholder="Search a venue here !"
+          value={venueName} onChange={this.inputChangeHandler}
+          className="SimpleSearchBox-input"/>
+        <i className="material-icons SimpleSearchBox-btn"
+          onClick={this.submitHandler}>
+          search
+        </i>
+      </div>
     );
   }
 }
+
+const SimpleSearchBox = withNotifications(_SimpleSearchBox);
